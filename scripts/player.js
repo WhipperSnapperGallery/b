471dcc8bbfad8785916025b6083cb090a887109f
musleho <musleho@sheridancollege.ca>
2023-05-18 11:02:21 -0400

    Added chat, updated vids, improved UI

--- scripts/player.js
+++ scripts/player.js
@@ -3,7 +3,6 @@ const videoIds = [];
 const videoPlayers = [];
 let currentVid = 0;
 let currentCarousel = "carousel0";
-
 function getLinks(callback) {
     const xhr = new XMLHttpRequest();
     xhr.onload = () => {
@@ -34,7 +33,7 @@ function createPlayers() {
     console.log(videoIds)
     for (let i = 1; i <= 2; i++) {
         const player = $(`#player${i}`);
-        player.attr("src", `https://www.youtube-nocookie.com/embed/${videoIds[i-1]}?controls=0&disablekb=1&enablejsapi=1&fs=0&modestbranding=1&playsinline=1&iv_load_policy=3`);
+        player.attr("src", `https://www.youtube-nocookie.com/embed/${videoIds[i - 1]}?controls=0&disablekb=1&enablejsapi=1&fs=0&modestbranding=1&playsinline=1&iv_load_policy=3`);
         videoPlayers[`player${i}`] = new YT.Player(`player${i}`, {
             events: {
                 'onReady': onPlayerReady
@@ -68,7 +67,7 @@ function previousVideo() {
 function stopLastVideo() {
     last = currentCarousel === "carousel0" ? $("#carousel1") : $("#carousel0");
     const videoPlayer = last.children('div').children('iframe').attr('id');
-    videoPlayers[videoPlayer].stopVideo();
+    videoPlayers[videoPlayer].pauseVideo();
 }
 
 function stopCurrentVideo() {
@@ -100,7 +99,51 @@ $(document).ready(() => {
     });
 
     $('#main-carousel').on('slid.bs.carousel', function () {
-        console.log("Stopping again!")
         stopLastVideo();
+        // Reset the chatbox
+        let firstMessage = "Are you in love?";
+        $("#chatbox").html(`<p class="botText"><span class=uText-label>Them:&nbsp;</span><span> ${firstMessage} </span></p>`);
+    });
+
+    let videoStream;
+    const videoElems = document.querySelectorAll('.webcam');
+
+    const modal = document.getElementById("videoModal");
+    modal.addEventListener("shown.bs.modal", function () {
+        checkingWCPermissionAndModalEngaged = true;
+        if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+            //Stop the current video
+            stopCurrentVideo();
+            // Access the webcam
+            navigator.mediaDevices.getUserMedia({ video: true })
+                .then(function (stream) {
+                    modal.classList.remove("d-none"); //Don't show the modal until
+                    // Store the video stream
+                    for (let videoElement of videoElems) {
+                        videoStream = stream;
+                        // Display the video stream on a video element
+                        videoElement.srcObject = stream;
+                        videoElement.play();
+                        checkingWCPermissionAndModalEngaged = false;
+                        errorOccurredWhileEngagingModal = false;
+                    }
+                })
+                .catch(function (error) {
+                    console.error('Error accessing webcam:', error);
+                });
+        } else {
+            console.error('getUserMedia is not supported by this browser.');
+        }
+    });
+
+    modal.addEventListener("hidden.bs.modal", function () {
+        if (videoStream) {
+            // Stop all tracks in the video stream
+            videoStream.getTracks().forEach(function (track) {
+                track.stop();
+            });
+            // Remove the video element
+            videoStream = null; // Reset the video stream
+        }
     });
 });
